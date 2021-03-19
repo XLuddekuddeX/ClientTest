e87ca9c99651e440838987b78a05f9d1ac757506
XLuddekuddeX <42523783+XLuddekuddeX@users.noreply.github.com>
2021-03-19 01:09:16 +0100

    Add project files.

---
+++ ClientTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Om anslutningen misslyckas kommer denna try/catch ge ett felmeddelande istället för att programmet avslutas. 
+            try
+            {
+                string address = "127.0.0.1";
+                int port = 8001;
+
+                //Försöker göra en anslutning till servern med hjälp av ip-adressen och porten. 
+                Console.WriteLine("Ansluter...");
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(address, port);
+                Console.WriteLine("Ansluten!");
+
+                //Ange meddelandet som ska skickas till servern. 
+                Console.WriteLine("Skriv in meddelande: ");
+                string message = Console.ReadLine();
+
+                //Omvandlar meddelandet "message" till Byte-formatet för att kunna skicka det. 
+                Byte[] bMessage = System.Text.Encoding.ASCII.GetBytes(message);
+
+                Console.WriteLine("Skickar...");
+                NetworkStream tcpStream = tcpClient.GetStream();
+                tcpStream.Write(bMessage, 0, bMessage.Length);
+
+                byte[] bRead = new byte[256];
+                int bReadSize = tcpStream.Read(bRead, 0, bRead.Length);
+
+                string read = "";
+                for (int i = 0; i < bReadSize; i++)
+                    read += Convert.ToChar(bRead[i]);
+                Console.WriteLine("Servern säger: " + read);
+
+                tcpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace TestServer
+{
+    class Program
+    {
+        static TcpListener tcpListener;
+
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelKeyPress);
+
+            IPAddress myIp = IPAddress.Parse("127.0.0.1");
+            tcpListener = new TcpListener(myIp, 8001);
+            tcpListener.Start();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Väntar på anslutning...");
+                    Socket socket = tcpListener.AcceptSocket();
+                    Console.WriteLine("Anslutningen accepterad från " + socket.RemoteEndPoint);
+
+                    Byte[] bMessage = new byte[256];
+                    int messageSize = socket.Receive(bMessage);
+                    Console.WriteLine("Meddelandet mottogs...");
+
+                    string message = "";
+                    for (int i = 0; i < messageSize; i++)
+                        message += Convert.ToChar(bMessage[i]);
+                    Console.WriteLine("Meddelande: " + message);
+
+                    Byte[] bSend = System.Text.Encoding.ASCII.GetBytes("Tack");
+                    socket.Send(bSend);
+                    Console.WriteLine("Svar skickat!");
+
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+        static void CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            tcpListener.Stop();
+            Console.WriteLine("Servern stängdes av!");  
+        }
+    }
+}
